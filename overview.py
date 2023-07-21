a07cfd0f58f7702c195bbffb1ad03c3e5da022c1
Pradnya Apte <114807672+Pradnya-Apte@users.noreply.github.com>
2023-07-21 10:37:06 +0530

    Add files via upload

---
+++ overview.py
@@ -0,0 +1,30 @@
+import datetime
+import cx_Oracle
+
+def all_time(investor):
+    #conn=''
+    #cursor = conn.cursor()
+
+    time=[x for x in range(2011,)]
+
+    today = datetime.date.today()
+    year = today.year
+    n=year + 1
+    yearly_value=[]
+
+    for i in range(2011,n):
+        query=('')
+        #cursor.execute('SELECT x, y FROM graph_data')
+        #data = cursor.fetchall()
+        #conn.close()
+
+        
+    
+
+
+all_time('RDA')
+
+
+
+
+
